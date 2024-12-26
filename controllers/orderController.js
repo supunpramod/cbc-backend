5a73da6349bcd231c532365afb3a72b25e2a27f2
Supun Pramod <pramodsupun06@gmail.com>
2024-12-26 22:19:50 +0530

    create orderrouter file

---
+++ controllers/orderController.js
@@ -0,0 +1,46 @@
+import Order from "../models/order.js";
+
+export async function createOrder(req,res){
+   // cbc001
+   //take the latest product id
+   if(!isCustomer){
+    res.json({
+        message:"Please login as customer to create orders"
+
+    })
+   }
+
+   try{
+    const latestOrder=await Order.find().sort
+    ({date:-1}).limit(1)
+
+    let orderId
+    if(latestOrder.length==0){
+        orderId="CBC001"
+    }else{
+        const currentOrderId=latestOrder[0].orderId
+        const numberString=currentOrderId.replace("CBC","")
+        const number=parseInt(numberString)
+        const newNumber=(number+1).toString().padStart(4,"0");
+        orderId="CBC"+ newNumber
+    }
+
+    const newOrderData=req.body
+    newOrderData.orderId=orderId
+    newOrderData.email=req.user.email
+
+    const order=new Order(newOrderData)
+    await order.save()
+
+    res.json({
+        message:"Order created"
+    })
+
+
+
+   }catch(error){
+    res.status(500).json({
+        message:error.message
+    })
+   }
+}
---
+++ routes/orderRouter.js
@@ -0,0 +1,6 @@
+import express from 'express';
+import { createOrder } from '../controllers/orderController';
+
+const orderRouter=express.Router();
+orderRouter.post("/",createOrder)
+ export default orderRouter;
