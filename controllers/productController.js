0a26bdb24b1764c5ffe43d27ddb94ece52539296
Supun Pramod <pramodsupun06@gmail.com>
2024-12-26 13:01:30 +0530

    create a product controller😊

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,4 +2,31 @@ import Product from "../models/product.js";
 
 export function createProduct(req,res){
 
-}
+    if(!isAdmin(req)){
+        res.json({
+            message:"Please login as administrator to add products"
+        })
+        return
+        }
+        const newProductData=req.body
+        const product=new Product(newProductData)
+        product.save().then(()=>{
+            res.json({
+                message:"Product created"
+            })
+        }).catch((error)=>{
+            res.json({
+                mesage:error
+            })
+        })
+    }
+
+export function getProducts(req,res){
+    Product.find({}).then((products)=>{
+        res.json(products)
+    })
+}    
+
+
+
+
